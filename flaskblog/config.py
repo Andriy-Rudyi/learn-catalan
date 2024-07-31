bcc19efde707cb7f3af1a4846bdd9ac0b0cfdc01
Andrew <andriyrudyy22@gmail.com>
2024-07-31 16:07:07 +0200

    The email was updated

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -11,7 +11,7 @@ class Config:
     #SQLALCHEMY_DATABASE_URI = DB_URL
 
 
-    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_SERVER = 'smtp-mail.outlook.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
 
