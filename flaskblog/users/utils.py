31e8e8d023bd24a25f0cf72877b3b7d63b943ad9
Andrew <andriyrudyy22@gmail.com>
2024-07-31 16:22:29 +0200

    The send_reset_email function fixing

--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -41,7 +41,7 @@ def save_picture(form_picture):
 def send_reset_email(user):
     token = user.get_reset_token()
     msg = Message('Password Reset Request', 
-                  sender='noreply@demo.com', 
+                  sender='admin@learn-catalan.com', 
                   recipients=[user.email])
     msg.body = f'''To reset your password, visit the following link:
 {url_for('users.reset_token', token=token, _external=True)}
