52094c3e30daeeda6743f002b5c161ce7c230978
Andrew <andriyrudyy22@gmail.com>
2024-08-20 23:31:12 +0200

    Comments added (migration included)

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -17,6 +17,7 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
+    comments = db.relationship('Comment', backref='author', lazy=True)
 
     def get_reset_token(self):
         s = Serializer(current_app.config['SECRET_KEY'])
@@ -43,6 +44,7 @@ class Post(db.Model):
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    comments = db.relationship('Comment', backref='post', lazy=True)
 
     def __repr__(self):
         return f"User('{self.title}', '{self.date_posted}')"
@@ -61,3 +63,11 @@ class PostView(ModelView):
 
     def is_accessible(self):
         return current_user.is_authenticated and current_user.username == 'Andriy'
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(250), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', on_delete="CASCADE"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id', on_delete="CASCADE"), nullable=False)
--- flaskblog/posts/routes.py
+++ flaskblog/posts/routes.py
@@ -2,13 +2,13 @@ from flask import (render_template, url_for, flash,
                    redirect, request, abort, Blueprint)
 from flask_login import current_user, login_required
 from flaskblog import db
-from flaskblog.models import Post
+from flaskblog.models import Post, Comment
 from flaskblog.posts.forms import PostForm
 
 posts = Blueprint('posts', __name__)
 
 
-@posts.route("/post/new", methods=['GET', 'POST'])
+@posts.route("/lesson/new", methods=['GET', 'POST'])
 @login_required
 def new_post():
     username = current_user.username
@@ -26,13 +26,13 @@ def new_post():
         flash('You do not have permission to do this!', 'danger')
         return redirect(url_for('main.home'))
 
-@posts.route("/post/<int:post_id>")
+@posts.route("/lesson/<int:post_id>")
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
 
 
-@posts.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@posts.route("/lesson/<int:post_id>/update", methods=['GET', 'POST'])
 @login_required
 def update_post(post_id):
     username = current_user.username
@@ -57,7 +57,7 @@ def update_post(post_id):
         return redirect(url_for('main.home'))
 
 
-@posts.route("/post/<int:post_id>/delete", methods=['POST'])
+@posts.route("/lesson/<int:post_id>/delete", methods=['POST'])
 @login_required
 def delete_post(post_id):
     username = current_user.username
@@ -72,3 +72,39 @@ def delete_post(post_id):
     else:
         flash('You do not have permission to do this!', 'danger')
         return redirect(url_for('main.home'))
+    
+@posts.route("/create-comment/<int:post_id>", methods=['POST'])
+@login_required
+def create_comments(post_id):
+    text = request.form.get('text')
+
+    if not text:
+        flash('Comment cannot be empty.', category='error')
+    else:
+        post = Post.query.get_or_404(post_id)
+        if post:
+            comment = Comment(text=text, author=current_user, post_id=post.id)
+            db.session.add(comment)
+            db.session.commit()
+        else:
+            flash('Post does not exist.', category='error')
+        
+    
+    return redirect(url_for('posts.post', post_id=post_id))
+
+
+@posts.route("/delete-comment/<int:comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.get_or_404(comment_id)
+    post_id = comment.post.id
+    if not comment:
+        flash('Comment does not exist.', category='error')
+        return redirect(url_for('main.home'))
+    elif comment.author.id != current_user.id and comment.post.author.id != current_user.id:
+        flash('You do not have permission to delete this comment.', category='error')
+    else:
+        db.session.delete(comment)
+        db.session.commit()
+
+    return redirect(url_for('posts.post', post_id=post_id))
